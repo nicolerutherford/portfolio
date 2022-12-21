5a80fe2e29e28afd226a131b7c2be4eeb7297ec5
nicolerutherford <61624209+nicolerutherford@users.noreply.github.com>
2022-12-21 21:41:15 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+//This is an array that will hold the file names! Add at least 4 images to the images array.
+var images = new Array();
+images[2] = "";
+//This is an array that holds the names of each location shown in your pictures.
+//Make sure that the index of each location is the same as the index of the picture in the images array
+var locations = new Array();
+locations[0] = "";
+
+//This is your starting index. First we start off at Home!
+var index = 0;
+
+//This function will help us toggle through our photos. Everytime the button is pressed it will call this function!
+function showImage() {
+
+}
